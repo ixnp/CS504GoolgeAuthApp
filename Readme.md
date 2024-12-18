b3f69aedb541b98851fc3f36e5b73c1518f1d5b6
root <root@ixWorkLaptop>
2024-12-17 17:43:56 -0800

    flask db set up with sqlalchemy

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#Citations
+https://www.geeksforgeeks.org/how-to-serve-static-files-in-flask/
--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask
-from flask import request
-from flask import render_template 
+#Connecting Flask app with SQLAlchemy (GeeksforGeeks, 2024a)
+from flask import Flask, request, redirect
+from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
-import json
 
-database = {}
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"Email : {self.email}, Password: {self.password}"
+
+# function to render index page
+@app.route('/')
 def index():
-    message = "Hey"
-    return render_template('index.html',  
-                           message=message) 
-  
-# run the application 
-if __name__ == "__main__": 
-    app.run(debug=True)
+    return render_template('index.html')
+
+@app.route('/user', methods=["POST"])
+def user():
+  email = request.form.get("email")
+  password = request.form.get("password")
+
+  user = User(email=email,password=password)
+  db.session.add(user)
+  db.session.commit()
+  return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
